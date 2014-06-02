7a697008632920e454a6b498ed7bc8c2482438bb
styrocupman <styrocupman@gmail.com>
2014-06-02 10:15:53 -0600

    code

---
+++ STGCodeChallenge2/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace STGCodeChallenge2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRoman = new System.Windows.Forms.TextBox();
+            this.txtValue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(91, 62);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 0;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Roman Numeral";
+            // 
+            // txtRoman
+            // 
+            this.txtRoman.Location = new System.Drawing.Point(16, 34);
+            this.txtRoman.Name = "txtRoman";
+            this.txtRoman.Size = new System.Drawing.Size(232, 22);
+            this.txtRoman.TabIndex = 2;
+            // 
+            // txtValue
+            // 
+            this.txtValue.Enabled = false;
+            this.txtValue.Location = new System.Drawing.Point(16, 120);
+            this.txtValue.Name = "txtValue";
+            this.txtValue.Size = new System.Drawing.Size(232, 22);
+            this.txtValue.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 99);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Value";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(262, 173);
+            this.Controls.Add(this.txtValue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRoman);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConvert);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRoman;
+        private System.Windows.Forms.TextBox txtValue;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ STGCodeChallenge2/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace STGCodeChallenge2
+{
+    public partial class Form1 : Form
+    {
+        protected string RomanNum;
+        protected int RomanValue;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            bool isNegative = false;
+            int cindx = 0;
+            RomanNum =  txtRoman.Text.ToUpper().Trim();
+            RomanValue = 0;
+
+            // Assumptions:
+            // According to modern rules, only one subtraction number is allowed (IX is valid IIX is not)
+            // For simplicity I am going to assume valid input for the most part and just ignore illegal chars (assign value 0)
+
+            // Check for 0
+            if (string.IsNullOrWhiteSpace(RomanNum))
+            {
+                txtValue.Text = "0";
+                return;
+            }
+
+            // Check for negative
+            if (getChar(0) == '-')
+            {
+                isNegative = true;
+                cindx = 1;
+            }
+
+            while (cindx < RomanNum.Length)
+            {
+                int val = getValue(getChar(cindx));
+                int nextVal = getValue(getChar(cindx+1));
+                // If the value of the char after the current one is greater, then we subtract the current char)
+                if (nextVal > val)
+                    RomanValue -= val;
+                else
+                    RomanValue += val;
+                cindx++;
+            }
+
+            // If the whole thing was preceded by a minus, it was a negative number
+            if (isNegative)
+                RomanValue *= -1;
+
+            txtValue.Text = RomanValue.ToString();
+        }
+
+        private char getChar(int cindx)
+        {
+            // Returns ' ' if we go past the end of the string
+            if (cindx >= RomanNum.Length)
+                return ' ';
+            else
+                return RomanNum[cindx];
+        }
+
+        private int getValue(char c)
+        {
+            switch (c)
+            {
+                case 'M': return 1000;
+                case 'D': return 500;
+                case 'C': return 100;
+                case 'L': return 50;
+                case 'X': return 10;
+                case 'V': return 5;
+                case 'I': return 1;
+                default:
+                    return 0;
+            }
+        }
+    }
+}
